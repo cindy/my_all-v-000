0578a13e777d2739613e9739ddeef19b0821f568
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-18 01:45:50 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,11 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  output = []
   while i < collection.size
-    yield(collection[i])
+    output << yield(collection[i])
     i += 1
   end
-  i == collection.size
+  !output.contains?(false)
+  
 end
